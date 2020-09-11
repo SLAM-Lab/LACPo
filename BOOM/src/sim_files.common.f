4f638f9caef3a06d33ddf0a3588f1018d922c378
ajaykrishna1111 <ajaykrishna1111@big.cerc.utexas.edu>
2020-09-10 23:32:18 -0500

    Adding edited file for GLS reference

---
+++ BOOM/src/sim_files.common.f
@@ -0,0 +1,10 @@
+
+/home/local/optimus/ajaykrishna1111/chipyard/sims/vcs/generated-src/example.TestHarness.MediumBoomConfig/AsyncResetReg.v
+/home/local/optimus/ajaykrishna1111/chipyard/sims/vcs/generated-src/example.TestHarness.MediumBoomConfig/EICG_wrapper.v
+/home/local/optimus/ajaykrishna1111/chipyard/sims/vcs/generated-src/example.TestHarness.MediumBoomConfig/plusarg_reader.v
+/home/local/optimus/ajaykrishna1111/chipyard/sims/vcs/generated-src/example.TestHarness.MediumBoomConfig/remote_bitbang.cc
+/home/local/optimus/ajaykrishna1111/chipyard/sims/vcs/generated-src/example.TestHarness.MediumBoomConfig/SimDTM.cc
+/home/local/optimus/ajaykrishna1111/chipyard/sims/vcs/generated-src/example.TestHarness.MediumBoomConfig/SimJTAG.cc
+/home/local/optimus/ajaykrishna1111/chipyard/sims/vcs/generated-src/example.TestHarness.MediumBoomConfig/SimSerial.cc
+/home/local/optimus/ajaykrishna1111/chipyard/sims/vcs/generated-src/example.TestHarness.MediumBoomConfig/SimSerial.v
+/home/local/optimus/ajaykrishna1111/chipyard/sims/vcs/generated-src/example.TestHarness.MediumBoomConfig/TestDriver.v
